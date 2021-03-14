ac0b88791e8be4d30c2ee35284d2925488683ad0
KarnaukhovKirill <kirya2361@gmail.com>
2021-03-14 14:24:47 +0300

    Конструктор с параметром size для батарейки

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,20 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery(int size) {
+        this.load = size;
+    }
+
+    public void exchange(Battery another) {
+        another.load = another.load + this.load;
+        this.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery first = new Battery(95);
+        Battery second = new Battery(2);
+        first.exchange(second);
+    }
+}
