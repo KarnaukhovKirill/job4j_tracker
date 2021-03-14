d8241e969cf745855162675d3e6c8a88834fd86e
KarnaukhovKirill <kirya2361@gmail.com>
2021-03-14 15:12:10 +0300

    Вычисление площади треугольника, используя ООП

---
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.oop;
+
+import static org.hamcrest.Matchers.closeTo;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class TriangleTest{
+
+    @Test
+    public void area8() {
+        Point a = new Point(0, 0);
+        Point b = new Point(4, 0);
+        Point c = new Point(0, 4);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        assertThat(rsl, closeTo(8, 0.001));
+    }
+
+    @Test
+    public void area9and5() {
+        Point a = new Point(2, 5);
+        Point b = new Point(0, 0);
+        Point c = new Point(5, 3);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        assertThat(rsl, closeTo(9.5, 0.001));
+    }
+}
