a61aaa42a5b8702818094d8e1db1687024cfa776
KarnaukhovKirill <kirya2361@gmail.com>
2021-03-19 17:48:03 +0300

    Переопределение метода String класса Object

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,9 +5,7 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
 
     public static void main(String[] args) {
-        Tracker tracker = new Tracker();
-        Item first = new Item(1, "First");
-        tracker.add(first);
-        System.out.println(tracker.findById(1).getName());
+        Item item = new Item(12, "Milk");
+        System.out.println(item.toString());
     }
 }
