4ec09eb6b096776a72eb51b6f1c08122e289947e
KarnaukhovKirill <kirya2361@gmail.com>
2021-03-23 21:42:31 +0300

    Повышающее приведение (up casting)

---
+++ src/main/java/ru/job4j/oop/HierarchyUsage.java
@@ -0,0 +1,10 @@
+package ru.job4j.oop;
+
+public class HierarchyUsage {
+
+    public static void main(String[] args) {
+        System.out.println(new Car());
+        System.out.println(new Bicycle());
+        System.out.println(new Object());
+    }
+}
