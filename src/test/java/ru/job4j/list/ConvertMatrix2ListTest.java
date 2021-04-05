dfda49db70f8155eb45f2f8f062e6577c52201e8
KarnaukhovKirill <kirya2361@gmail.com>
2021-04-05 11:04:35 +0300

    Конвертация двухмерного массива в список

---
+++ src/test/java/ru/job4j/list/ConvertMatrix2ListTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.list;
+
+import org.junit.Test;
+import java.util.Arrays;
+import java.util.List;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ConvertMatrix2ListTest {
+    @Test
+    public void when2on2ArrayThenList4() {
+        ConvertMatrix2List list = new ConvertMatrix2List();
+        int[][] input = {
+                {1, 2},
+                {3, 4}
+        };
+        List<Integer> expect = Arrays.asList(
+                1, 2, 3, 4
+        );
+        List<Integer> result = list.toList(input);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void when4on2ArrayThenList7() {
+        ConvertMatrix2List list = new ConvertMatrix2List();
+        int[][] input = {
+                {1, 2, 7, 8, 9},
+                {3, 4}
+        };
+        List<Integer> expect = Arrays.asList(
+                1, 2, 7, 8, 9, 3, 4
+        );
+        List<Integer> result = list.toList(input);
+        assertThat(result, is(expect));
+    }
+}
