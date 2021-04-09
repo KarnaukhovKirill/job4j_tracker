3ee0abc7de7dc57ab9c15a397639c8306e34707c
KarnaukhovKirill <kirya2361@gmail.com>
2021-04-09 18:44:41 +0300

    Исправления

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -3,7 +3,7 @@ package ru.job4j.bank;
 import java.util.*;
 
 public class BankService {
-    private static Map<User, List<Account>> users = new HashMap<>();
+    private Map<User, List<Account>> users = new HashMap<>();
 
     public void addUser(User user) {
         users.putIfAbsent(user, new ArrayList<>());
@@ -20,25 +20,27 @@ public class BankService {
     }
 
     public User findByPassport(String passport) {
+        User result = null;
         for (User u : users.keySet()) {
             if (passport.equals(u.getPassport())) {
-                return u;
+                result = u;
             }
         }
-        return null;
+        return result;
     }
 
     public Account findByRequisite(String passport, String requisite) {
+        Account result = null;
         User newUser = findByPassport(passport);
         List<Account> listOfAccounts = users.get(newUser);
         if (listOfAccounts != null) {
             for (Account acc : listOfAccounts) {
                 if (acc.getRequisite().equals(requisite)) {
-                    return acc;
+                    result = acc;
                 }
             }
         }
-        return null;
+        return result;
     }
 
     public boolean transferMoney(String srcPassport, String srcRequisite,
@@ -46,10 +48,12 @@ public class BankService {
         boolean rsl = false;
         Account srcAcc = findByRequisite(srcPassport, srcRequisite);
         Account destAcc = findByRequisite(destPassport, destRequisite);
-        if (srcAcc.getBalance() >= amount) {
-            srcAcc.setBalance(srcAcc.getBalance() - amount);
-            destAcc.setBalance(destAcc.getBalance() + amount);
-            rsl = true;
+        if (srcAcc != null && destAcc != null) {
+            if (srcAcc.getBalance() >= amount) {
+                srcAcc.setBalance(srcAcc.getBalance() - amount);
+                destAcc.setBalance(destAcc.getBalance() + amount);
+                rsl = true;
+            }
         }
         return rsl;
     }
