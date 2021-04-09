373300cf16b4b289f824e2bc163234fd54576927
KarnaukhovKirill <kirya2361@gmail.com>
2021-04-09 13:26:12 +0300

    Исправления

--- src/main/java/ru/job4j/collection/PassportOffice.java
+++ src/main/java/ru/job4j/collection/PassportOffice.java
@@ -7,11 +7,11 @@ public class PassportOffice {
     private Map<String, Citizen> citizens = new HashMap<>();
 
     public boolean add(Citizen citizen) {
-        boolean rsl = citizens.containsKey(citizen.getPassport());
-        if (!rsl) {
+        if (!citizens.containsKey(citizen.getPassport())) {
             citizens.put(citizen.getPassport(), citizen);
+            return true;
         }
-        return !rsl;
+        return false;
     }
 
     public Citizen get(String passport) {
