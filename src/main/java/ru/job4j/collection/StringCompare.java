9116b7a32e2e4b06eda8e2e24a084dc48afb9822
KarnaukhovKirill <kirya2361@gmail.com>
2021-04-14 11:24:37 +0300

    Исравления

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -5,17 +5,13 @@ import java.util.Comparator;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        int rsl = 0;
         int count = Math.max(left.length(), right.length());
-        for (int index = 0; index < count; index++) {
+        for (int index = 0; index < count - 1; index++) {
+            int rsl = Character.compare(left.charAt(index), right.charAt(index));
             if (rsl != 0) {
                 return rsl;
             }
-            if ((index + 1) == count) {
-                return Integer.compare(left.length(), right.length());
-            }
-            rsl = Character.compare(left.charAt(index), right.charAt(index));
         }
-        return rsl;
+        return Integer.compare(left.length(), right.length());
     }
 }
