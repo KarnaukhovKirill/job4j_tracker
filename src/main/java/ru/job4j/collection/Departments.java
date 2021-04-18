aed6d4c433b38979f11ada3bc2a95b9ada98cb7d
KarnaukhovKirill <kirya2361@gmail.com>
2021-04-18 23:23:57 +0300

    Класс Departments

---
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -0,0 +1,31 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class Departments {
+    public static List<String> fillGaps(List<String> deps) {
+        Set<String> tmp = new LinkedHashSet<>();
+        for (String value : deps) {
+            String start = "";
+            for (String el : value.split("/")) {
+                tmp.add(start + el);
+                start += el + "/";
+            }
+        }
+        return new ArrayList<>(tmp);
+    }
+
+    public static void sortAsc(List<String> orgs) {
+        Collections.sort(orgs);
+    }
+
+    public static void sortDesc(List<String> orgs) {
+        orgs.sort(new DepDescComp());
+    }
+
+    public static void main(String[] args) {
+        List<String> input = Arrays.asList("k1/sk1/ssk1");
+        List<String> rsl = fillGaps(input);
+        System.out.println(rsl);
+    }
+}
