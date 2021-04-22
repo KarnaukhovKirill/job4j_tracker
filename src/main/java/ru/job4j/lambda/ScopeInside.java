237ce5ceb6c6f0e444f9f32dba133196b7f24700
KarnaukhovKirill <kirya2361@gmail.com>
2021-04-22 21:30:15 +0300

    Исправления

--- src/main/java/ru/job4j/lambda/ScopeInside.java
+++ src/main/java/ru/job4j/lambda/ScopeInside.java
@@ -6,11 +6,10 @@ public class ScopeInside {
     public static void main(String[] args) {
         int[] number = {1, 2, 3};
         int total = 0;
-        for (int i = 0; i < number.length; i++) {
-            int num = number[i];
+        for (Integer i : number) {
             int anotherTotal = total;
             total = add(
-                    () -> anotherTotal + num
+                    () -> anotherTotal + i
             );
         }
         System.out.println(total);
