2358479f51d39bc4fb300d16ea3f062807c32609
KarnaukhovKirill <kirya2361@gmail.com>
2021-04-22 22:11:30 +0300

    Stream API

---
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -0,0 +1,14 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamUsage {
+    public static void main(String[] args) {
+        List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 7, 8);
+        List<Integer> filterList = list.stream().filter(
+                integer -> integer % 2 == 0
+        ).collect(Collectors.toList());
+        filterList.forEach(System.out::println);
+    }
+}
