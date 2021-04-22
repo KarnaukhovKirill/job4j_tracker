523095e9f7ca70725c9debf8e624808b6a57d5d8
KarnaukhovKirill <kirya2361@gmail.com>
2021-04-22 23:07:38 +0300

    Метод составления адресов клиентов с помощью Stream.map()

---
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ProfilesTest {
+
+    @Test
+    public void testDefaultCollect() {
+        Profiles method = new Profiles();
+        List<Profile> profiles = List.of(
+                new Profile(new Address("Moscow", "Arbat", 1, 2)),
+                new Profile(new Address("London", "Baker Street", 51, 1)),
+                new Profile(new Address("Archangelsk", "Suvorov Street", 55, 100))
+        );
+        List<Address> rsl = method.collect(profiles);
+        List<Address> expected = new ArrayList<>();
+        expected.add(new Address("Moscow", "Arbat", 1, 2));
+        expected.add(new Address("London", "Baker Street", 51, 1));
+        expected.add(new Address("Archangelsk", "Suvorov Street", 55, 100));
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void testAddSimpleAddress() {
+        Profiles method = new Profiles();
+        List<Profile> profiles = List.of(
+                new Profile(new Address("Moscow", "Arbat", 1, 2)),
+                new Profile(new Address("London", "Baker Street", 51, 1)),
+                new Profile(new Address("Archangelsk", "Suvorov Street", 55, 100)),
+                new Profile(new Address("Archangelsk", "Suvorov Street", 55, 100))
+        );
+        List<Address> rsl = method.collect(profiles);
+        List<Address> expected = new ArrayList<>();
+        expected.add(new Address("Moscow", "Arbat", 1, 2));
+        expected.add(new Address("London", "Baker Street", 51, 1));
+        expected.add(new Address("Archangelsk", "Suvorov Street", 55, 100));
+        expected.add(new Address("Archangelsk", "Suvorov Street", 55, 100));
+        assertThat(rsl, is(expected));
+    }
+}
