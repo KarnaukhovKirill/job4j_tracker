5e0f9152e4cda82f7265079c830a5ea7756246e8
KarnaukhovKirill <kirya2361@gmail.com>
2021-04-23 20:38:51 +0300

    Исправления

--- src/main/java/ru/job4j/stream/StreamUsage01.java
+++ src/main/java/ru/job4j/stream/StreamUsage01.java
@@ -3,11 +3,11 @@ package ru.job4j.stream;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class StreamUsage {
+public class StreamUsage01 {
     public static void main(String[] args) {
-        List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 7, 8);
+        List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 7, 8, -2, 0);
         List<Integer> filterList = list.stream().filter(
-                integer -> integer % 2 == 0
+                integer -> integer % 2 == 0 && integer > 0
         ).collect(Collectors.toList());
         filterList.forEach(System.out::println);
     }
