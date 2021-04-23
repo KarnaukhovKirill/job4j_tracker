87cfe7b7aff4dd96257bc6f773deee25be68e445
KarnaukhovKirill <kirya2361@gmail.com>
2021-04-23 21:09:35 +0300

    Добавление метода distinct для удаления дубликотов и метода sort для сортировки финального списка

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,12 +1,17 @@
 package ru.job4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
+
     public List<Address> collect(List<Profile> profiles) {
+        Comparator<Address> compAscCityName = (o1, o2) -> o1.getCity().compareTo(o2.getCity());
         return profiles.stream()
                 .map(profile -> profile.getAddress())
+                .sorted(compAscCityName)
+                .distinct()
                 .collect(Collectors.toList());
     }
 }
--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -20,9 +20,9 @@ public class ProfilesTest {
         );
         List<Address> rsl = method.collect(profiles);
         List<Address> expected = new ArrayList<>();
-        expected.add(new Address("Moscow", "Arbat", 1, 2));
-        expected.add(new Address("London", "Baker Street", 51, 1));
         expected.add(new Address("Archangelsk", "Suvorov Street", 55, 100));
+        expected.add(new Address("London", "Baker Street", 51, 1));
+        expected.add(new Address("Moscow", "Arbat", 1, 2));
         assertThat(rsl, is(expected));
     }
 
@@ -37,10 +37,27 @@ public class ProfilesTest {
         );
         List<Address> rsl = method.collect(profiles);
         List<Address> expected = new ArrayList<>();
-        expected.add(new Address("Moscow", "Arbat", 1, 2));
+        expected.add(new Address("Archangelsk", "Suvorov Street", 55, 100));
         expected.add(new Address("London", "Baker Street", 51, 1));
+        expected.add(new Address("Moscow", "Arbat", 1, 2));
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void testAddSimpleCityName() {
+        Profiles method = new Profiles();
+        List<Profile> profiles = List.of(
+                new Profile(new Address("Moscow", "Arbat", 1, 2)),
+                new Profile(new Address("London", "Baker Street", 51, 1)),
+                new Profile(new Address("Archangelsk", "Suvorov Street", 55, 100)),
+                new Profile(new Address("Archangelsk", "Suvorov Street", 30, 10))
+        );
+        List<Address> rsl = method.collect(profiles);
+        List<Address> expected = new ArrayList<>();
         expected.add(new Address("Archangelsk", "Suvorov Street", 55, 100));
-        expected.add(new Address("Archangelsk", "Suvorov Street", 55, 100));
+        expected.add(new Address("Archangelsk", "Suvorov Street", 30, 10));
+        expected.add(new Address("London", "Baker Street", 51, 1));
+        expected.add(new Address("Moscow", "Arbat", 1, 2));
         assertThat(rsl, is(expected));
     }
 }
