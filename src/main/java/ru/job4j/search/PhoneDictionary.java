33066df97a5cc384a028f7607c1c26858f8bc4d2
KarnaukhovKirill <kirya2361@gmail.com>
2021-04-27 10:22:58 +0300

    Использование ключевого слова var

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -15,29 +15,17 @@ public class PhoneDictionary {
         Predicate<Person> containSurname = f -> f.getSurname().contains(key);
         Predicate<Person> containPhone = f -> f.getPhone().contains(key);
         Predicate<Person> containAddress = f -> f.getAddress().contains(key);
-        Predicate<Person> combine =  containName
+        Predicate<Person> combine = containName
                 .or(containSurname)
                 .or(containPhone)
                 .or(containAddress);
         ArrayList<Person> result = new ArrayList<>();
-        for (Person person : persons) {
+        for (var person : persons) {
             if (combine.test(person)) {
                 result.add(person);
             }
         }
         return result;
     }
-/*
-    public ArrayList<Person> find(String key) {
-        ArrayList<Person> result = new ArrayList<>();
-        for (Person a : persons) {
-            if (a.getName().contains(key) || a.getSurname().contains(key)
-                    || a.getPhone().contains(key) || a.getAddress().contains(key)) {
-                result.add(a);
-            }
-        }
-        return result;
-
- */
-    }
+}
 
